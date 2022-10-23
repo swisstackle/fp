7d794f58f315f98a870418faf241ea8893f290f8
Alain Schaerer <alainsch@buffalo.edu>
2022-10-23 17:56:40 -0400

    Detecting when shoulder is tilted.

--- testproject/main.py
+++ testproject/main.py
@@ -1,5 +1,6 @@
 import cv2 as cv2
 import mediapipe as mp
+import numpy as np
 
 if __name__ == "__main__":
     mp_drawing = mp.solutions.drawing_utils
@@ -14,6 +15,22 @@ if __name__ == "__main__":
             results = pose.process(image)
             image.flags.writeable = True
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+                leftshoulder = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value]
+                rightshoulder = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
+                difference = rightshoulder.z - leftshoulder.z
+                if(np.absolute(difference) > 0.4):
+                    print(" Shoulder Tilted")
+                else:
+                    print("Shoulder Not Tilted")
+
+               #print('{0:.16f}'.format(rightshoulder.x - leftshoulder.x))
+
+            except:
+                pass
+
             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                       mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
                                       mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
